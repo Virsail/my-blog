45c6a37061edeaff2c2474ba559123f4a240953a
virsail <virsaileric@gmail.com>
2020-09-29 19:50:15 +0300

    import requests to request.py

---
+++ app/models.py
@@ -0,0 +1,20 @@
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@600&display=swap');
+
+body {
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    /*background: #230e4a;*/
+    z-index: 1;
+    background: linear-gradient(to bottom right, #230e4a, black);
+    font-family: 'Josefin Slab', serif;
+}
+
+#particles-js {
+    height: auto;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    z-index: 1;
+}
